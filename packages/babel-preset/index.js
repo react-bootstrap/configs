dfbb114f0d02e0867e2f283d599f9a9e56f849a7
kyletsang <6854874+kyletsang@users.noreply.github.com>
2021-05-07 08:55:24 -0700

    remove esmodules target

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -34,7 +34,6 @@ module.exports = (
         modules,
         ignoreBrowserslistConfig: true,
         targets: {
-          esmodules: true,
           browsers: browserlist,
         },
       },
